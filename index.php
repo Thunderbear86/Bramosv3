dc1e99616699f9762ca278e8d6784c46c0855085
Thorbjørn <benj434d@edu.zealand.dk>
2023-10-25 10:59:54 +0200

    transfer

--- index.php
+++ index.php
@@ -1,8 +1,88 @@
-<?php
-require "settings/init.php";
+<!-- Instruktion til webbrowser om at vi kører HTML5 -->
+<!DOCTYPE html>
 
-$blogs = $db->sql("SELECT * FROM blogs");
+<!-- html starter og slutter hele dokumentet / lang=da: Fortæller siden er på dansk -->
+<html lang="da">
 
-foreach ($blogs as $blog){
-    echo $blog->blogName . "<br>";
-}
+<!-- I <head> har man opsætning - det ser brugeren ikke, men det fortæller noget om siden -->
+<head>
+    <!-- Sætter tegnsætning til utf-8 som bl.a. tillader danske bogstaver -->
+    <meta charset="utf-8">
+
+    <!-- Titel som ses oppe i browserens tab mv. -->
+    <title>Metaperspektiv - Din pædagogiske vidensbank</title>
+
+    <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <!-- Sikrer man kan benytte CSS ved at tilkoble en CSS fil -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse - bliver brugt til responsive websider -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<!-- i <body> har man alt indhold på siden som brugeren kan se -->
+<body>
+
+<header>
+    <?php include 'navbar.php';?>
+</header>
+
+<div class="container velkomst">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Metaperspektiv - Din pædagogiske vidensbank</h1>
+        </div>
+    </div>
+</div>
+
+<div class="container systemer">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Systemer</h2>
+        </div>
+    </div>
+</div>
+
+<div class="container apps">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Apps</h2>
+        </div>
+    </div>
+</div>
+
+<div class="container kurser">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Kurser</h2>
+        </div>
+    </div>
+</div>
+
+<div class="container om">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Om Metaperspektiv</h2>
+        </div>
+    </div>
+</div>
+
+<div class="container blog">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Vidensbank</h2>
+        </div>
+    </div>
+</div>
+
+<footer></footer>
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,82 @@
+<?php echo '
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid"> 
+
+        <!-- Brand Section -->
+        <div class="col">
+            <a class="navbar-brand fw-bold" href="index.php">Metaperspektiv</a>
+        </div>
+
+        <!-- Menu Items Section -->
+        <div class="col">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="servicesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Systemer
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="servicesDropdown1">
+                            <a class="dropdown-item" href="#">Dokumentation</a>
+                            <a class="dropdown-item" href="#">Kommunikation</a>
+                            <a class="dropdown-item" href="#">Videns Database</a>
+                        </div>
+                    </li>
+                    
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="servicesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            App
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="servicesDropdown2">
+                            <a class="dropdown-item" href="#">Personale</a>
+                            <a class="dropdown-item" href="#">Borger</a>
+                            <a class="dropdown-item" href="#">Adm. og Ledelse</a>
+                        </div>
+                    </li>
+                    
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="servicesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Kurser
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="servicesDropdown3">
+                            <a class="dropdown-item" href="#">Personale</a>
+                            <a class="dropdown-item" href="#">Borger</a>
+                            <a class="dropdown-item" href="#">Adm. og Ledelse</a>
+                        </div>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Vidensbank</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Vores Vision</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kontakt Os</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- Search + Buttons Section -->
+        <div class="col d-flex justify-content-end g-2"> 
+            <form class="form-inline d-flex mr-2">
+                <input class="form-control mr-sm-2" type="search" placeholder="Søg" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Søg</button>
+            </form>
+            <a href="#" class="btn btn-primary">Book Møde</a>
+        </div>
+
+    </div>
+</nav>
+
+<!-- Include Bootstrap JS and Popper.js for dropdowns and other functionality -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+';
+?>
